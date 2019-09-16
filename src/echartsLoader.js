b737e7b5c80108046876b45827af5545c66566c5
502218 <810158465@qq.com>
2019-09-16 16:22:58 +0800

    添加版本映射

--- src/echartsLoader.js
+++ src/echartsLoader.js
@@ -1,7 +1,11 @@
 import scriptjsLoader from '@sdp.nd/js-async-loader'
+import { echartMap } from './map'
+
 export default async function echartsLoader (versions, modules) {
   const uri = `//cdnjs.cloudflare.com/ajax/libs/echarts/:versions/:moduleName.js`
   versions = versions || '4.0.2'
+  versions = echartMap[versions] || echartMap[versions.match(/(?<![.\d])\d+(?=(\.\d+)*\.\d+)/)[0]]
+
   modules = modules || ['echarts.min']
   for (let i = 0; i < modules.length; i++) {
     await scriptjsLoader(uri, null, {
--- src/hchartsLoader.js
+++ src/hchartsLoader.js
@@ -1,7 +1,12 @@
 import scriptjsLoader from '@sdp.nd/js-async-loader'
+import { hchartMap } from './map'
+
 export default async function hchartsLoader (versions, modules) {
   const uri = `//cdnjs.cloudflare.com/ajax/libs/highcharts/:versions/:moduleName.js`
+
   versions = versions || '6.0.7'
+  versions = hchartMap[versions] || hchartMap[versions.match(/(?<![.\d])\d+(?=(\.\d+)*\.\d+)/)[0]]
+
   modules = modules || ['highcharts']
   if (!modules.includes('js/modules/oldie') && navigator.userAgent.indexOf('MSIE 8.0') > 0) {
     modules.push('js/modules/oldie')
